b5843cd61a362e42df82943f05a8975d30cac1de
Christopher Moser <moser121@icloud.com>
2018-02-27 22:00:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments){
   var word = [];
-  for (var i = 0; i <= musicians.length; i++){
+  for (var i = 0; i < musicians.length; i++){
     word.push(musicians[i] + 'plays' + instruments[i])
   }
 }
